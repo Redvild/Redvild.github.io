356d03ba5e205494f127ec7fd3d4cb4df296d255
Redvild <32127453+Redvild@users.noreply.github.com>
2017-10-18 14:05:48 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,21 @@
+﻿<?
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone_number'])&&$_POST['phone_number']!="")){
+        $to = redvild@gmail.com';
+        $subject = 'Envato User';
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Name: '.$_POST['name'].'</p>
+                        <p>Phone: '.$_POST['phone_number'].'</p>     
+			<p>E-mail: '.$_POST['email'].'</p>
+                        <p>Username: '.$_POST['username'].'</p>                    
+                    </body>
+                </html>'; 
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n";
+        $headers .= "From: Отправитель <from@example.com>\r\n";
+        mail($to, $subject, $message, $headers);
+}
+?>
