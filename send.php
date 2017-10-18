7db9b3f37cc830b65cb5837379276306e97f6bb5
Redvild <32127453+Redvild@users.noreply.github.com>
2017-10-18 15:57:58 +0300

    Update send.php

--- send.php
+++ send.php
@@ -14,8 +14,8 @@
                         <p>Username: '.$_POST['username'].'</p>                    
                     </body>
                 </html>'; 
-        $headers  = "Content-type: text/html; charset=utf-8 \r\n";
-        $headers .= "From: Отправитель <from@example.com>\r\n";
+        $headers = "Content-type: text/html; charset=utf-8 \r\n";
+        $headers = "From: Отправитель <from@example.com>\r\n";
         mail($to, $subject, $message, $headers);
 }
 ?>
